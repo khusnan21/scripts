ba579e3af2d56557991726fa353ba7dbeb18c51e
Jago Gardiner (nysadev) <nysadev@raphielgang.org>
2019-07-26 03:44:25 +0100

    treewide: Clang doesn't like GCC10 alongside
    Signed-off-by: Jago Gardiner (nysadev) <nysadev@raphielgang.org>

--- dronesetup.sh
+++ dronesetup.sh
@@ -4,7 +4,7 @@ cd ..
 git clone https://github.com/nysadev/acrux-ak3 -b master anykernel
 
 if [[ "$@" =~ "clang"* ]]; then
-        git clone https://github.com/RaphielGang/aarch64-raph-linux-android -b elf --depth=1 gcc
+	git clone https://github.com/RaphielGang/aarch64-linux-gnu-8.x.git --depth=1 gcc
         git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi/ -b 240719 --depth=1 gcc32
 	git clone https://github.com/RaphielGang/aosp-clang --depth=1 clang
 elif [[ "$@" =~ "gcc10"* ]]; then
