9b5f26e23ca9dbce9cb508d705ef738881a32bbc
Jago Gardiner (nysadev) <nysadev@raphielgang.org>
2019-07-26 02:22:42 +0100

    drone: gcc 10 memes
    Signed-off-by: Jago Gardiner (nysadev) <nysadev@raphielgang.org>

--- dronesetup.sh
+++ dronesetup.sh
@@ -7,6 +7,9 @@ if [[ "$@" =~ "clang" ]]; then
 	git clone https://github.com/RaphielGang/aarch64-linux-gnu-8.x.git --depth=1 gcc
 	git clone https://github.com/RaphielGang/arm-linux-gnueabi-8.x.git --depth=1 gcc32
 	git clone https://github.com/RaphielGang/aosp-clang --depth=1 clang
+elif [[ "$@" =~ "gcc10" ]]; then
+	git clone https://github.com/baalajimaestro/aarch64-maestro-linux-android -b 200719 --depth=1 gcc
+	git clone https://github.com/baalajimaestro/arm-maestro-linux-gnueabi/ -b 240719 --depth=1 gcc32
 else
 	git clone https://github.com/kdrag0n/aarch64-elf-gcc -b 9.x --depth=1 gcc
 	git clone https://github.com/kdrag0n/arm-eabi-gcc -b 9.x --depth=1 gcc32
