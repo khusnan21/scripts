3a32bedefe998e3e5d4a2d385a6d989427faacab
Jago Gardiner (nysadev) <nysadev@raphielgang.org>
2019-07-26 03:03:54 +0100

    kernel: Some more GCC 10 memes
    Signed-off-by: Jago Gardiner (nysadev) <nysadev@raphielgang.org>

--- kernel.sh
+++ kernel.sh
@@ -56,7 +56,7 @@ make O=out ARCH=arm64 acrux_defconfig
 if [[ "$@" =~ "clang"* ]]; then
         make -j${KEBABS} O=out ARCH=arm64 CC=clang CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="/drone/src/gcc/bin/aarch64-linux-gnu-" CROSS_COMPILE_ARM32="/drone/src/gcc32/bin/arm-linux-gnueabi-"
 elif [[ "$@" =~ "gcc10"* ]]; then
-	make -j${KEBABS} O=out ARCH=arm64 CROSS_COMPILE="/drone/src/gcc/bin/aarch64-maestro-linux-gnu-" CROSS_COMPILE_ARM32="/drone/src/gcc32/bin/arm-maestro-linux-gnueabi-"
+	make -j${KEBABS} O=out ARCH=arm64 CROSS_COMPILE="/drone/src/gcc/bin/aarch64-raphiel-elf-" CROSS_COMPILE_ARM32="/drone/src/gcc32/bin/arm-maestro-linux-gnueabi-"
 else
 	make -j${KEBABS} O=out ARCH=arm64 CROSS_COMPILE="/drone/src/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="/drone/src/gcc32/bin/arm-eabi-"
 fi
