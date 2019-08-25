07bfe22990e74f5cf48f67ceb8db63cc9fa28514
nysascape <nysadev@raphielgang.org>
2019-08-25 23:09:02 +0000

    kernel: some bruh for daisy
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# We're building Acrux.
+# We're building Procyon.
 cd ..
 
 # Export compiler type
@@ -17,17 +17,10 @@ fi
 # Export correct version
 if [[ "$@" =~ "beta"* ]]; then
 	export TYPE=beta
-	export VERSION="Acrux-BETA-${RELEASE_VERSION}-rc${DRONE_BUILD_NUMBER}-${RELEASE_CODENAME}"
-	# Be careful if something changes LOCALVERSION line
-        sed -i "50s/.*/CONFIG_LOCALVERSION=\"-Acrux-${RELEASE_VERSION}-rc${DRONE_BUILD_NUMBER}-${RELEASE_CODENAME}\"/g" arch/arm64/configs/acrux_defconfig
-	export INC="$(echo ${RC} | grep -o -E '[0-9]+')"
-	INC="$((INC + 1))"
-	sed -i "2s/.*/rc$INC/g" CURRENTVERSION
+	export VERSION="Procyon-Daisy-BETA-${RELEASE_VERSION}-rc${DRONE_BUILD_NUMBER}-${RELEASE_CODENAME}"
 else
 	export TYPE=stable
-	export VERSION="Acrux-Stable-${RELEASE_VERSION}-${RELEASE_CODENAME}"
-        # Be careful if something changes LOCALVERSION line
-        sed -i "50s/.*/CONFIG_LOCALVERSION=\"-Acrux-${RELEASE_VERSION}-${RELEASE_CODENAME}\"/g" arch/arm64/configs/acrux_defconfig
+	export VERSION="Procyon-Daisy-Stable-${RELEASE_VERSION}-${RELEASE_CODENAME}"
 fi
 
 export ZIPNAME="${VERSION}.zip"
@@ -39,22 +32,22 @@ if [[ -z "${KEBABS}" ]]; then
 fi
 
 # Post to CI channel
-curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Kernel: <code>Acrux Kernel</code>
+curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Kernel: <code>Procyon Kernel</code>
 Type: <code>${TYPE}</code>
-Device: <code>MI 8 Lite (platina)</code>
+Device: <code>MI A2 Lite (daisy)</code>
 Compiler: <code>${COMPILER}</code>
 Branch: <code>$(git rev-parse --abbrev-ref HEAD)</code>
 Latest Commit: <code>$(git log --pretty=format:'%h : %s' -1)</code>
 Changelog: ${DRONE_REPO_LINK}/compare/$DRONE_COMMIT_BEFORE...$DRONE_COMMIT_AFTER
 <i>Build started on Drone Cloud...</i>
-Check the build status here: https://cloud.drone.io/nysascape/acrux/${DRONE_BUILD_NUMBER}" -d chat_id=${CI_CHANNEL_ID} -d parse_mode=HTML
+Check the build status here: https://cloud.drone.io/nysascape/procyon_kernel_daisy/${DRONE_BUILD_NUMBER}" -d chat_id=${CI_CHANNEL_ID} -d parse_mode=HTML
 curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Build started for revision ${DRONE_BUILD_NUMBER}" -d chat_id=${KERNEL_CHAT_ID} -d parse_mode=HTML
 
 # Make is shit so I have to pass thru some toolchains
 # Let's build, anyway
 PATH="/drone/src/clang/bin:${PATH}"
 START=$(date +"%s")
-make O=out ARCH=arm64 acrux_defconfig
+make O=out ARCH=arm64 procyon_defconfig
 if [[ "$@" =~ "clang"* ]]; then
         make -j${KEBABS} O=out ARCH=arm64 CC=clang CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="/drone/src/gcc/bin/aarch64-linux-gnu-" CROSS_COMPILE_ARM32="/drone/src/gcc32/bin/arm-maestro-linux-gnueabi-"
 elif [[ "$@" =~ "gcc10"* ]]; then
@@ -75,10 +68,10 @@ zip -r9 ${ZIPNAME} *
 CHECKER=$(ls -l ${ZIPNAME} | awk '{print $5}')
 
 if (($((CHECKER / 1048576)) > 5)); then
-	curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Build compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds for Platina" -d chat_id=${CI_CHANNEL_ID} -d parse_mode=HTML
+	curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Build compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds for daisy (PLATINA GUYS DO NOT FLASH)" -d chat_id=${CI_CHANNEL_ID} -d parse_mode=HTML
 	curl -F chat_id="${CI_CHANNEL_ID}" -F document=@"$(pwd)/${ZIPNAME}" https://api.telegram.org/bot${BOT_API_KEY}/sendDocument
 else
-	curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Error in build!!" -d chat_id=${CI_CHANNEL_ID}
+	curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Error in build!" -d chat_id=${CI_CHANNEL_ID}
 	exit 1;
 fi
 
